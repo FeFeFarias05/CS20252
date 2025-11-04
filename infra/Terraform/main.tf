d01b977389d34f881472a08c019a6369bdc007c6
ana.lopes <ana.lopes011@edu.pucrs.br>
2025-11-04 15:38:13 -0300

    ajustes yml

--- infra/Terraform/main.tf
+++ infra/Terraform/main.tf
@@ -232,3 +232,7 @@ output "jwks_uri" {
 output "jwt_audience" {
   value = local.jwt_audience
 }
+
+output "cognito_login_url" {
+  value = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.main.id}&response_type=code&scope=openid&redirect_uri=${var.callback_url}"
+}
