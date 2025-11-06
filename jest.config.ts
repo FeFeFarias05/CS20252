274f41e3b7e94a0c2c6a5746502da962b7fd9d63
Fernanda Farias <fernanda.farias05@hotmail.com>
2025-11-06 00:34:04 -0300

    npm

--- jest.config.ts
+++ jest.config.ts
@@ -3,25 +3,20 @@ import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'node',
 
-  // Transforma arquivos TS para que Jest entenda
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 
-  // Aqui estão os testes
   testMatch: ['**/__tests__/**/*.test.ts'],
 
-  // ✅ Permite Jest transformar ES Modules no node_modules (ex: jose, node-fetch)
   transformIgnorePatterns: ['/node_modules/(?!jose|node-fetch)/'],
 
-  // ✅ Se quiser mockar por padrão alguns módulos
   moduleNameMapper: {
-    '^@/lib/(.*)$': '<rootDir>/src/lib/$1', // suporte ao @ alias
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
   },
 
-  // ✅ Carrega variáveis de ambiente antes dos testes
   setupFiles: ['dotenv/config'],
-  
+
 setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
 };
